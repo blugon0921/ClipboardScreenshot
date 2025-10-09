9be6c25591b88460ede71fe4d30a6372bfce0a20
blugon0921 <blugon0921@gmail.com>
2025-10-09 16:21:43 +0900

    :sparkles: v1.0.5-1.21.10

--- src/main/kotlin/kr/blugon/clipboardscreenshot/ClipboardScreenshot.kt
+++
@@ -1,9 +0,0 @@
-package kr.blugon.clipboardscreenshot
-
-import net.fabricmc.api.ModInitializer
-
-class ClipboardScreenshot : ModInitializer {
-
-    override fun onInitialize() {
-    }
-}
--- src/main/kotlin/kr/blugon/clipboardscreenshot/client/ClipboardScreenshotClient.kt
+++ src/main/kotlin/kr/blugon/clipboardscreenshot/client/ClipboardScreenshotClient.kt
@@ -9,6 +9,7 @@ import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper
 import net.minecraft.client.MinecraftClient
 import net.minecraft.client.option.KeyBinding
 import net.minecraft.client.texture.NativeImage
+import net.minecraft.client.toast.SystemToast
 import net.minecraft.client.util.InputUtil
 import net.minecraft.client.util.ScreenshotRecorder
 import net.minecraft.text.Text
@@ -21,65 +22,67 @@ import java.awt.datatransfer.UnsupportedFlavorException
 import java.awt.image.BufferedImage
 
 var wasKeyPressed = false
-class ClipboardScreenshotClient : ClientModInitializer {
-    val screenshotCopyKey: KeyBinding = KeyBindingHelper.registerKeyBinding(KeyBinding(
-        "key.screenshot.copy",
-        InputUtil.Type.KEYSYM,
-        GLFW.GLFW_KEY_F2,
-        KeyBinding.Category.MISC
-    ))
+val screenshotCopyKey: KeyBinding = KeyBindingHelper.registerKeyBinding(KeyBinding(
+    "key.screenshot.copy",
+    InputUtil.Type.KEYSYM,
+    GLFW.GLFW_KEY_F2,
+    KeyBinding.Category.MISC
+))
 
-
-    override fun onInitializeClient() {
-        ClientTickEvents.END_CLIENT_TICK.register { client ->
-            val window = client.window
-            val isPressed = InputUtil.isKeyPressed(
-                window,
-                KeyBindingHelper.getBoundKeyOf(screenshotCopyKey).code)
-            if(!isPressed) {
-                wasKeyPressed = false
-                return@register
-            }
-            if (wasKeyPressed) return@register
-            wasKeyPressed = true
-            val player = client.player
-            if(GraphicsEnvironment.isHeadless()) {
-                player?.sendMessage(Text.translatable("screenshot.clipboard.error"), false)
-                return@register
-            }
-            val instance = MinecraftClient.getInstance()
-            val buffer = instance.framebuffer
-            ScreenshotRecorder.takeScreenshot(buffer) { image ->
-                CoroutineScope(Dispatchers.Default).launch {
-                    copyToClipboard(image.toBufferedImage())
-                    instance.execute {
-                        player?.sendMessage(Text.translatable("screenshot.copied"), false)
-                    }
+fun initClient() {
+    ClientTickEvents.END_CLIENT_TICK.register { client ->
+        val isPressed = InputUtil.isKeyPressed(
+            client.window,
+            KeyBindingHelper.getBoundKeyOf(screenshotCopyKey).code
+        )
+        if(!isPressed) {
+            wasKeyPressed = false
+            return@register
+        }
+        if (wasKeyPressed) return@register
+        wasKeyPressed = true
+        if(GraphicsEnvironment.isHeadless()) {
+            client.sendMessageOrToast(Text.translatable("screenshot.clipboard.error"))
+            return@register
+        }
+        val instance = MinecraftClient.getInstance()
+        val buffer = instance.framebuffer
+        ScreenshotRecorder.takeScreenshot(buffer) { image ->
+            CoroutineScope(Dispatchers.Default).launch {
+                copyToClipboard(image.toBufferedImage())
+                instance.execute {
+                    client.sendMessageOrToast(Text.translatable("screenshot.copied"))
                 }
             }
         }
     }
+}
 
-    fun NativeImage.toBufferedImage(): BufferedImage {
-        return BufferedImage(this.width, this.height, BufferedImage.TYPE_INT_ARGB).also {
-            for (x in 0 until this.width) {
-                for (y in 0 until this.height) {
-                    it.setRGB(x, y, this.getColorArgb(x, y))
-                }
+fun MinecraftClient.sendMessageOrToast(text: Text) {
+    if(player == null) toastManager.add(
+        SystemToast.create(this, SystemToast.Type.NARRATOR_TOGGLE, text, Text.of(""))
+    ) else player!!.sendMessage(text, false)
+}
+
+fun NativeImage.toBufferedImage(): BufferedImage {
+    return BufferedImage(this.width, this.height, BufferedImage.TYPE_INT_ARGB).also {
+        for (x in 0 until this.width) {
+            for (y in 0 until this.height) {
+                it.setRGB(x, y, this.getColorArgb(x, y))
             }
         }
     }
+}
 
-    fun copyToClipboard(image: BufferedImage) {
-        val transferableImage = object: Transferable {
-            override fun getTransferDataFlavors(): Array<DataFlavor> = arrayOf(DataFlavor.imageFlavor)
-            override fun isDataFlavorSupported(flavor: DataFlavor): Boolean = flavor == DataFlavor.imageFlavor
-            override fun getTransferData(flavor: DataFlavor): Any {
-                if (!isDataFlavorSupported(flavor)) throw UnsupportedFlavorException(flavor)
-                return image
-            }
+fun copyToClipboard(image: BufferedImage) {
+    val transferableImage = object: Transferable {
+        override fun getTransferDataFlavors(): Array<DataFlavor> = arrayOf(DataFlavor.imageFlavor)
+        override fun isDataFlavorSupported(flavor: DataFlavor): Boolean = flavor == DataFlavor.imageFlavor
+        override fun getTransferData(flavor: DataFlavor): Any {
+            if (!isDataFlavorSupported(flavor)) throw UnsupportedFlavorException(flavor)
+            return image
         }
-        val clipboard = Toolkit.getDefaultToolkit().systemClipboard
-        clipboard.setContents(transferableImage, null)
     }
+    val clipboard = Toolkit.getDefaultToolkit().systemClipboard
+    clipboard.setContents(transferableImage, null)
 }
